25e021c555502e07722d26554dffc7521b1a9644
ahincho <ahincho@unsa.edu.pe>
2023-09-17 16:17:20 -0500

    Handling Feign Exception when Person isnt on Apis Net Reniec

--- src/main/java/com/unsa/reniec/application/services/ReniecForeignServiceOpenFeign.java
+++ src/main/java/com/unsa/reniec/application/services/ReniecForeignServiceOpenFeign.java
@@ -1,7 +1,9 @@
 package com.unsa.reniec.application.services;
 
 import com.unsa.reniec.domain.dtos.ReniecApisNetResponse;
+import feign.FeignException;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -19,8 +21,12 @@ public class ReniecForeignServiceOpenFeign implements ReniecForeignService {
 
     @Override
     public PersonResponseDto getPersonByDocument(String document) {
-        ReniecApisNetResponse reniecApisNetResponse = this.reniecApisNetOpenFeignClient.getPersonByDocument(bearerToken, document);
-        return mapReniecApisNetResponseToPersonResponseDto(reniecApisNetResponse);
+        try {
+            ReniecApisNetResponse reniecApisNetResponse = this.reniecApisNetOpenFeignClient.getPersonByDocument(bearerToken, document);
+            return mapReniecApisNetResponseToPersonResponseDto(reniecApisNetResponse);
+        } catch (FeignException feignException) {
+            return null;
+        }
     }
 
     private static PersonResponseDto mapReniecApisNetResponseToPersonResponseDto(ReniecApisNetResponse response) {
